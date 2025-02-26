f073ba8bcd4d5a48c135528afbf8cec94ba26cb2
Bruno Pereyra <brunopereyra459@gmail.com>
2025-02-26 14:56:10 -0300

    antiguedad de msjs

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(
-		// cors.Config{
-		// 	AllowCredentials: true,
-		// 	AllowOrigins:     "https://www.pinkker.tv,https://pinkker.tv",
-		// 	AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
-		// 	AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-		// },
+		cors.Config{
+			AllowCredentials: true,
+			AllowOrigins:     "https://www.pinkker.tv,https://pinkker.tv",
+			AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
+			AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		},
 	))
 
 	routes.Routes(app, redisClient, newMongoDB)
